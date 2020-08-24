43053c02b98e591a6bb68dac872897097514e79c
zhanysch <zhanysch@gmail.com>
2020-08-24 21:49:45 +0600

    arbeit

--- app/src/main/java/com/example/asynctascretrofit/model/ForecastDays/ForCastItemNumbeZeroFourth.kt
+++ app/src/main/java/com/example/asynctascretrofit/model/ForecastDays/ForCastItemNumbeZeroFourth.kt
@@ -3,7 +3,7 @@ package com.example.asynctascretrofit.model.ForecastDays
 data class ForCastItemNumbeZeroFourth(
     val dt : Int,
     val visibility : Int,
-    val pop : Int,
+    val pop : Double,
     val dt_txt : String,
     val  main : ForCastMainFifth
 
